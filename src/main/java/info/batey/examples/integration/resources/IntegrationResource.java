e4a36ad50a6c372ea3fe6908c2959ad5391dcdce
Christopher Batey <christopher.batey@gmail.com>
2014-08-04 21:59:46 +0100

    adding an aysnc endpoint

--- src/main/java/info/batey/examples/integration/resources/IntegrationResource.java
+++ src/main/java/info/batey/examples/integration/resources/IntegrationResource.java
@@ -5,9 +5,11 @@ import info.batey.examples.integration.points.auth.PinCheckDependency;
 import info.batey.examples.integration.points.device.DeviceServiceDependency;
 import info.batey.examples.integration.points.user.UserServiceDependency;
 import org.apache.http.client.HttpClient;
+import rx.Observable;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import java.util.concurrent.Future;
 
 @Path("integrate")
 public class IntegrationResource {
@@ -25,5 +27,16 @@ public class IntegrationResource {
         String device = new DeviceServiceDependency(httpClient).execute();
         Boolean pinCheck = new PinCheckDependency(httpClient).execute();
         return String.format("[User info: %s] \n[Device info: %s] \n[Pin check: %s] \n", user, device, pinCheck);
+    }
+
+    @GET
+    @Timed
+    @Path("async")
+    public String integrateAsync() throws Exception {
+        Future<String> user = new UserServiceDependency(httpClient).queue();
+        Future<String> device = new DeviceServiceDependency(httpClient).queue();
+        Future<Boolean> pinCheck = new PinCheckDependency(httpClient).queue();
+
+        return String.format("[User info: %s] \n[Device info: %s] \n[Pin check: %s] \n", user.get(), device.get(), pinCheck.get());
     }
 }
